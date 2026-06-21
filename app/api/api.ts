2397e3b8fd8820b00d8333c899d03190425a6ed3
Disendra Yadav <disendra123@gmail.com>
2026-06-21 23:16:05 +0530

    changes

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,8 +1,4 @@
-// export const API_BASE_URL = "https://scorecareapp.com/api";
-
-
-
-export const API_BASE_URL = "http://localhost:5000";
-
+export const API_BASE_URL = "https://scorecareapp.com/api";
+// export const API_BASE_URL = "http://localhost:5000";
 export const PLAY_STORE_URL =
   "https://play.google.com/apps/internaltest/4701307504694712853";
