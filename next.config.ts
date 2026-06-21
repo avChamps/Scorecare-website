dccbb7f7f2603232186269aaa5cd96f8620c9414
Disendra Yadav <disendra123@gmail.com>
2026-06-22 00:29:46 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: false,
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true,
   },
